a27571439ce7363dbd6d4083813d8ffd79aff65f
fernandoAOliveira <fernandous4@yahoo.com.br>
2020-07-26 20:09:52 -0300

    add routes e pequenas alterações de nome

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'static_pages/home'
+
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
+  resources :users
+
+  resources :statements
+  post 'statements/report' => 'statements#report'
+
+  resources :accounts do
+    member do
+      post :deposit
+      post :withdraw
+      post :transfer
+      post :close_account
+    end
+  end
+
+  root 'static_pages#home'
 end
